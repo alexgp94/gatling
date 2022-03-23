6edd6c2d42de0bf2c7659eb4971f9297deaedcb4
alexgp94 <alexcgp94@gmail.com>
2022-03-23 00:17:48 -0500

    Products Prod

--- src/test/scala/com/gatling/simulations/ProductsSM.scala
+++ src/test/scala/com/gatling/simulations/ProductsSM.scala
@@ -94,7 +94,7 @@ class ProductsSM extends Simulation {
       }
   }
     setUp(
-      Scenarios.highPurchase
+      Scenarios.default
         .inject(rampUsers(userCount) during (testDuration seconds)).protocols(httpProtocol),
       //    Scenarios.highPurchase
       //      .inject(rampUsers(5) during (10.seconds)).protocols(httpProtocol)
